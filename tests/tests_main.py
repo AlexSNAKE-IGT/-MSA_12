d8e5eee84d01e1d06913439af57bf8f17bfaa145
unknown <alexgamechanel2001@gmail.com>
2023-04-06 05:42:03 +0300

    Added tests

---
+++ tests/tests_main.py
@@ -0,0 +1,10 @@
+import requests
+
+api_url = 'http://localhost:8000'
+
+
+class TestBooks():
+    def test_get_not_empty_books(self):
+        response = requests.get(f'{api_url}/books')
+        assert response.status_code == 200
+        assert len(response.json()) == 3
