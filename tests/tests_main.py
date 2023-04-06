a336cb6d49f048c540f9b818b80b543920ff3a0d
unknown <alexgamechanel2001@gmail.com>
2023-04-06 06:34:56 +0300

    Added function test

--- tests/tests_main.py
+++ tests/tests_main.py
@@ -2,6 +2,9 @@ import requests
 
 api_url = 'http://localhost:8000'
 
+def test_healthcheck():
+    response = requests.get(f'{api_url}/__health')
+    assert response.status_code == 200
 
 class TestBooks():
     def test_get_empty_books(self):
